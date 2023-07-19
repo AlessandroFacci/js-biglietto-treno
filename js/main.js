6b47b34aba18ed52a7a98e29d2f695b06e73b1a3
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-19 17:31:12 +0200

    html and js: add final price

--- js/main.js
+++ js/main.js
@@ -20,11 +20,19 @@ console.log(totalPrice);
 if (passengerAge < 18) {
   const discountPrice20 = (totalPrice - 20 / 100).toFixed(2);
   console.log(discountPrice20);
+}
 
-  //altrimenti se il passeggero ha un'età >= 65 anni
-} else if (passengerAge >= 65) {
+//altrimenti se il passeggero ha un'età >= 65 anni
+else if (passengerAge >= 65) {
   const discountPrice40 = (totalPrice - 40 / 100).toFixed(2);
   console.log(discountPrice40);
 }
 //altrimenti paga il prezzo pieno
 else totalPrice;
+
+//mostro a schermo prezzo finale
+document.getElementById("final-price").innerHTML = totalPrice;
+
+//document.getElementById("final-price20").innerHTML = discountPrice20;
+
+//document.getElementById("final-price40").innerHTML = discountPrice40;
